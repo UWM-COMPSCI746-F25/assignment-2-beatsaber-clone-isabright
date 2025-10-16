053220bf29307d030010df2fe308ddf9c6b10001
Isabelle Bright <131718387+isabright@users.noreply.github.com>
2025-10-16 00:07:44 -0500

    x and y random coordinates

--- spawner.gd
+++ spawner.gd
@@ -4,6 +4,8 @@ extends Node3D
 
 var time_accum = 0.0
 var next_time = randf_range(0.5, 2.0)
+var x_rand
+var y_rand
 
 func _process(delta):
 	time_accum += delta
@@ -11,4 +13,8 @@ func _process(delta):
 		time_accum = 0.0
 		next_time = randf_range(1.0, 5.0)
 		var instance = left_box.instantiate()
+		x_rand = randf_range(-0.7, 0.5)
+		y_rand = randf_range(-0.5, 0.7)
+		var change = Vector3(x_rand, y_rand, 0)
+		instance.global_position += change
 		add_child(instance)
