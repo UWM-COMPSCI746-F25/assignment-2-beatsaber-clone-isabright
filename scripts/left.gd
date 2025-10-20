73df7f9997549e0561267a976747e4c484c99cfa
Isabelle Bright <131718387+isabright@users.noreply.github.com>
2025-10-20 18:50:34 -0500

    swords disappear now!

--- scripts/left.gd
+++ scripts/left.gd
@@ -1,9 +1,15 @@
 extends XRController3D
-
+var active = true
 
 func _on_button_pressed(name: String):
-	if name == "ax_button":
-		# Hide the CSGCylinder3D 
-		$"Node3D/Area3D/LeftCyllinder".height = 0.001
-		# Disable CollisionShape3D collision safely
-		$"Node3D/Area3D/LeftCollision".disabled = true
+	print("Button pressed:", name)
+	if name == "select_button":
+		print("left x button pressed")
+		if active == true:
+			$"Node3D/Area3D/LeftCyllinder".height = 0
+			$"Node3D/Area3D/LeftCollision".disabled = true
+			active = !active
+		else:
+			$"Node3D/Area3D/LeftCyllinder".height = 1
+			$"Node3D/Area3D/LeftCollision".disabled = false
+			active = !active
--- scripts/right.gd
+++ scripts/right.gd
@@ -1,9 +1,17 @@
 extends XRController3D
 
+var active = true
 
 func _on_button_pressed(name: String):
-	if name == "ax_button":
-		# Hide the CSGCylinder3D 
-		$"Node3D/Area3D/RightCyllinder".height = 0.001
-		# Disable CollisionShape3D collision safely
-		$"Node3D/Area3D/RightCollision".disabled = true
+	print("Button pressed:", name)
+	if name == "select_button":
+		print("right a button pressed")
+		
+		if active == true:
+			$"Node3D/Area3D/RightCyllinder".height = 0
+			$"Node3D/Area3D/RightCollision".disabled = true
+			active = !active
+		else:
+			$"Node3D/Area3D/RightCyllinder".height = 1
+			$"Node3D/Area3D/RightCollision".disabled = false
+			active = !active
